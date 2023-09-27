42912a5c8dec031c2149b3efa0e13379bd45ba69
esmond-adjei <xmongram@gmail.com>
2023-09-27 04:09:52 +0000

    modified some files

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,15 +4,20 @@ import csv
 import requests
 import sys
 
-if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+if __name__ == "__main__":
+    if len(sys.argv) < 1:
+        print(f"Usage: {sys.argv[0]} <user_id>")
+        sys.exit(1)
+
+    user_id = sys.argv[1]
+    URL = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(URL + f"users/{user_id}").json()
+    username = user.get("username")
+    todos = requests.get(URL + "todos", params={"userId": user_id}).json()
+
+    with open(f"{user_id}.csv", "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow(
-            [user_id, username, t.get("completed"), t.get("title")]
-         ) for t in todos]
+        for task in todos:
+            writer.writerow([user_id, username, task.get("completed"),
+                            task.get("title")])
--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -4,16 +4,21 @@ import json
 import requests
 import sys
 
-if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    with open("{}.json".format(user_id), "w") as jsonfile:
+if __name__ == "__main__":
+    if len(sys.argv) < 1:
+        print(f"Usage: {sys.argv[0]} <user_id>")
+        sys.exit(1)
+
+    user_id = sys.argv[1]
+    URL = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(URL + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(URL + "todos", params={"userId": user_id}).json()
+
+    with open(f"{user_id}.json", "w") as jsonfile:
         json.dump({user_id: [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
+                "task": task.get("title"),
+                "completed": task.get("completed"),
                 "username": username
-            } for t in todos]}, jsonfile)
+            } for task in todos]}, jsonfile)
