78c0872d22e1087acfcd922518a2c08ab5b187c0
esmond-adjei <xmongram@gmail.com>
2023-10-10 20:08:11 +0000

    modified 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,36 +1,33 @@
 #!/usr/bin/python3
 """
- a recursive function that queries the Reddit API
- and returns a list containing the titles of all
- hot articles for a given subreddit
+recursively get the hot list
 """
 
 import requests
 
 
 def recurse(subreddit, hot_list=[], after=None):
-    """
-     returns a list containing titles of all hot articles
-     for a given subreddit.
-    """
+    """recursively get hot list"""
 
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {"User-Agent": "Custom"}
-    params = {"limit": 100}
-
-    if after:
-        params["after"] = after
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=100'
+    params = {'limit': 100, 'after': after} if after else {'limit': 100}
+    headers = {'User-Agent': 'MyRedditBot'}
 
     response = requests.get(url, headers=headers, params=params)
+
     if response.status_code == 200:
-        data = response.json().get("data")
-        after = data.get("after")
+        data = response.json().get('data')
+        children = data.get('children')
 
-        if after:
-            hot_list.extend([child.get("data").get("title")
-                            for child in data.get("children")])
+        if children:
+            hot_list.extend([child.get('data').get('title')
+                            for child in children])
+            after = data.get('after')
 
-            return recurse(subreddit, hot_list, after)
+            if after:
+                return recurse(subreddit, hot_list, after)
+            else:
+                return hot_list
         else:
             return hot_list
     else:
