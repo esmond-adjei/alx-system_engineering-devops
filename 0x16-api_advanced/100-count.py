a08026038d4828b9b2e5a21b5145dc8255acacab
esmond-adjei <xmongram@gmail.com>
2023-10-10 20:18:08 +0000

    modified 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -5,13 +5,7 @@ import requests
 
 
 def count_words(subreddit, word_list, word_counts=None, after=None):
-    """
-     a recursive function that queries the Reddit API,
-     parses the title of all hot articles, and prints a
-     sorted count of given keywords (case-insensitive,
-     delimited by spaces. Javascript should count
-     as javascript, but java should not
-    """
+    """count all keywords in articles"""
 
     if word_counts is None:
         word_counts = {}
@@ -19,7 +13,7 @@ def count_words(subreddit, word_list, word_counts=None, after=None):
     url = f'https://www.reddit.com/r/{subreddit}/hot.json'
     params = {'limit': 100, 'after': after} if after else {'limit': 100}
 
-    headers = {'User-Agent': 'Custom'}
+    headers = {'User-Agent': 'MyRedditBot'}
 
     response = requests.get(url, headers=headers, params=params)
 
@@ -35,7 +29,7 @@ def count_words(subreddit, word_list, word_counts=None, after=None):
                 word = word.lower()
                 if title.count(word) > 0:
                     word_counts[word] = word_counts.get(word, 0)\
-                                        + title.count(word)
+                        + title.count(word)
 
         if after:
             return count_words(subreddit, word_list, word_counts, after)
