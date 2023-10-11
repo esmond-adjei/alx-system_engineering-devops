99c69ca9ba3c0c1950d3cd6990871690d51359df
Esmond Adjei <81225469+esmond-adjei@users.noreply.github.com>
2023-10-11 20:46:27 +0000

    removed service reuirememt

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -3,10 +3,3 @@ exec { 'modify_wp_settings':
   command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
   path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/'
 }
-
-# Ensure Apache is running with the correct configuration
-service { 'apache2':
-  ensure => 'running',
-  enable => true,
-  require => Exec['modify_wp_settings'],
-}
