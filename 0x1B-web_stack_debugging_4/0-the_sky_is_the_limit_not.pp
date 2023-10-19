c090ea2e3a6b2d36211ce32405fb4e9b16147549
esmond-adjei <xmongram@gmail.com>
2023-10-19 22:03:33 +0000

    modified some files

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # Increase the ULIMIT of the default file
 exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  command => 'sed -i "s/15/1000/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 } ->
 
